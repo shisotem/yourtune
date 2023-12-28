685ae4e00d8fdcbb587521040cc05feaf52243b6
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-28 12:50:05 +0900

    feat: App component skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <span>YourTune</span>
+
+      <section className="content">
+        <h1 className="content-title">YouTube to MP3, Simple!</h1>
+        <p className="content-description">
+          Transform YouTube videos into MP3 files in just a few clicks.
+        </p>
+
+        <form className="form">
+          <input
+            placeholder="Paste a YouTube video URL link..."
+            className="form-input"
+            type="text"
+          />
+          <button type="submit" className="form-button">
+            Search
+          </button>
+        </form>
+
+        <a href="" className="download-btn">
+          Download MP3
+        </a>
+      </section>
+    </div>
+  );
 }
 
 export default App;
